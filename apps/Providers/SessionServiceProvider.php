15edbe379a54ea7ee0e1a4d039c82245d67fea65
baichou <baichou@qingmutec.com>
2018-01-08 11:25:48 +0800

    队列功能

---
+++ apps/Providers/SessionServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 明月有色
+ * Date: 2017/12/25
+ * Time: 17:19
+ */
+
+namespace Apps\Providers;
+
+use Framework\Providers\ServiceProvider;
+use Phalcon\Session\Adapter\Files;
+
+class SessionServiceProvider extends ServiceProvider
+{
+    protected $serviceName = 'session';
+
+    /**
+     * Register application service.
+     *
+     * @return mixed
+     */
+    public function register()
+    {
+        $this->di->setShared($this->serviceName,function (){
+            $session = new Files();
+            $session->start();
+            return $session;
+        });
+    }
+}
